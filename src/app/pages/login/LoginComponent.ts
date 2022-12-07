923a7520b138e21031c0ebb5e6ed048b9b73f23d
Rogério GASTAO <gastaorogerio53@gmail.com>
2022-12-07 18:43:41 +0100

    commit 4

---
+++ src/app/pages/login/LoginComponent.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth-service/auth.service';
+import { LoginModel } from 'src/app/models/LoginModel';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent implements OnInit {
+
+  loginmodel!: LoginModel;
+  isDisabled: boolean = true;
+  loginForm!: FormGroup;
+  loginModel: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+
+    private auth: AuthService) { }
+
+  ngOnInit(): void {
+    // this.loginForm = this.formBuilder.group(
+    //   {
+    //     email: ['', [Validators.required, Validators.email]],
+    //     motdepasse: ['', [Validators.required]],
+    //   }
+
+    // );
+    this.loginmodel = new LoginModel({
+      email: "", motdepasse: { pwd: "", confirm_pwd: "" },
+      terms: false
+
+    });
+
+    // this.loginmodel = new LoginModel{}
+  }
+
+
+  controle(): void {
+    if (!this.loginmodel.motdepasse.pwd || !this.loginmodel.email) {
+      this.isDisabled = true;
+    }
+    else (this.loginmodel.motdepasse.pwd && this.loginmodel.email)
+    {
+      this.isDisabled = false;
+    }
+  }
+
+  submitLogin(): void {
+    debugger; //Pour voire si ça marche
+    var donnesLogin = this.loginForm.getRawValue();
+  }
+
+  onContinue(): void {
+    this.auth.login();
+    this.router.navigateByUrl('pageblog');
+    }
+
+  CreerUncompte(): void {
+    this.router.navigateByUrl('CreerUncompte');
+  }
+
+  //   public onSubmit({ value, valid}: { value: LoginModel, valid: boolean }) {
+  //     this.loginmodel = value;
+  //     console.log( this.loginmodel);
+  //     console.log("valid: " + valid);
+  // }
+
+}
+
