44a25ab2c9f4c6866bfae8f7c85584e569648575
Rogério GASTAO <gastaorogerio53@gmail.com>
2022-12-09 17:08:29 +0100

    Delete LoginComponent.ts file
     * Replace with login.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/LoginComponent';
+import { LoginComponent } from './pages/login/login.component';
 import { PageBlogComponent } from './pages/login/page-blog/page-blog.component';
 import { CommonModule } from '@angular/common';
 import { CreerUncompteComponent } from './creer-uncompte/creer-uncompte.component';
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,1 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth-service/auth.service';
+import { LoginModel } from 'src/app/models/LoginModel';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+export class LoginComponent implements OnInit {
+
+  loginmodel: LoginModel;
+  isDisabled: boolean = true;
+  loginModel: any;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+
+    private auth: AuthService) { }
+
+  ngOnInit(): void {
+    // this.loginForm = this.formBuilder.group(
+    //   {
+    //     email: ['', [Validators.required, Validators.email]],
+    //     motdepasse: ['', [Validators.required]],
+    //   }
+
+    // );
+    this.loginmodel = new LoginModel({
+      email: '', motdepasse: { pwd: '' },
+      terms: false
+    });
+
+    // this.loginmodel = new LoginModel{}
+  }
+
+
+
+  controle(): void {
+    console.log('ici controle()', this.loginmodel.email, this.loginmodel.motdepasse);
+    if (!this.loginmodel.motdepasse.pwd || !this.loginmodel.email) {
+      this.isDisabled = true;
+
+    } else {
+      this.isDisabled = false;
+      // const pattern = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm);
+    }
+  }
+
+  submitLogin(): void {
+    // debugger; // Pour voire si ça marche
+    // const donnesLogin = this.loginForm.getRawValue();
+  }
+
+  onContinue(): void {
+    this.auth.login();
+    this.router.navigateByUrl('pageblog');
+
+  }
+
+  creerUncompte(): void {
+    this.router.navigateByUrl('CreerUncompte');
+    sessionStorage.setItem(this.loginmodel.email, this.loginmodel.motdepasse.pwd);
+
+    // getemail(): void {
+    //   return sessionStorage.getItem(this.loginmodel.email);
+    // }
+
+    // getmotdepasse() {
+    //   sessionStorage.getItem(this.loginmodel.motdepasse.pwd);
+    // }
+
+  }
+
+
+
+  //   public onSubmit({ value, valid}: { value: LoginModel, valid: boolean }) {
+  //     this.loginmodel = value;
+  //     console.log( this.loginmodel);
+  //     console.log("valid: " + valid);
+  // }
+
+  // Stocker le mot de passe
+
+}
 
