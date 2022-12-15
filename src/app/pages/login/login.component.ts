730f20218c0f90c96677cba651fc56cb35fd718b
Rogério GASTAO <gastaorogerio53@gmail.com>
2022-12-15 14:29:19 +0100

    Revert "commit 7"
    This reverts commit 172e7f6601d4579428f126a4b34f959fc696315e.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -20,6 +20,7 @@ export class LoginComponent implements OnInit {
   constructor(
         private formBuilder: FormBuilder,
         private router: Router,
+
         private auth: AuthService) { }
 
   ngOnInit(): void {
