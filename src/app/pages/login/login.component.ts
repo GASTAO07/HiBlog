46647ae00dca9233071ae31921fa274e5be5ca98
Rogério GASTAO <gastaorogerio53@gmail.com>
2022-12-15 14:14:36 +0100

    essaye1

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -28,7 +28,6 @@ export class LoginComponent implements OnInit {
     //     email: ['', [Validators.required, Validators.email]],
     //     motdepasse: ['', [Validators.required]],
     //   }
-
     // );
     this.loginmodel = new LoginModel({
       email: '', motdepasse: { pwd: '' },
