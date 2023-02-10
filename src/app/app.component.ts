0409f4643a61b88990936b3fee4b4b8864956ee4
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-02-10 15:59:30 +0100

    Delete the logic on the ngOninit of app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginValidationService } from './service/auth-service/login-validation-service.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  implements OnInit{
-  isUserLoggedIn: boolean = false;
-
   constructor(
-    private loginValidationService: LoginValidationService,
   ) { }
 
   ngOnInit(): void {
-    console.log('debutappcomp is user : ', this.isUserLoggedIn);
-    this.isUserLoggedIn = this.loginValidationService.isLoggedIn();
-    console.log('finappcomp is user : ', this.isUserLoggedIn);
   }
 }
