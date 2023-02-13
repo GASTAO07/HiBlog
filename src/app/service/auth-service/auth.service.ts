c3eda15525a08423fd02c05aa9e93b2ce7ce7fdb
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-02-13 14:05:39 +0100

    Adding getUser and setUser to show in the screen the user

--- src/app/service/auth-service/auth.service.ts
+++ src/app/service/auth-service/auth.service.ts
@@ -8,9 +8,20 @@ export class AuthService {
 
   login(): void {
     this.token = 'MyFakeToken';
-    // sLoggedIn sera définie sur true lorsque la méthode de connexion sera appelée et l'utilisateur pourra voir le composant de la barre d'outils.
     localStorage.setItem('isLoggedIn', 'true');
     console.log('authici', 'isLoggedIn');
+  }
+
+  logout(): void {
+    localStorage.removeItem('isLoggedIn');
+  }
+
+  setUser(user: any): void {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser(): any {
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   getToken(): string {
