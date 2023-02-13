4f8d010d866cb789ee1ee0a61327e1a81e3ca876
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-02-13 14:04:17 +0100

    Adding canActivateGuard for the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageBlogComponent } from './pages/login/page-blog/page-blog.component';
+import { PageBlogComponent } from './page-blog/page-blog.component';
 import { CreerUncompteComponent } from './creer-uncompte/creer-uncompte.component';
 import { AuthGuard } from './guards/auth.guard';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 
 const routes: Routes = [
