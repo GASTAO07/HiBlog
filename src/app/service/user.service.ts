f7ac0aeeb0f3055b7460809ae7067bb0e3338a24
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-02-15 11:03:55 +0100

    Implement setUser fonction

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -7,10 +7,40 @@ import { User } from 'src/app/interfaces/user.interface';
 export class UserService {
 
   setUser(user: User): void {
-    localStorage.setItem('user', JSON.stringify(user));
+    // 1- Récupérer le tableau d'utilisateurs
+    const users: User[] = this.getUsers();
+
+    // 2- Ajouter mon utilisateur passé en paramètre au tableau
+    users.push(user);
+
+    // 3- Sauvegarder le nouveau tableau dans le LocalStorage
+    this.setUsers(users);
+
   }
 
-  getUser(): User {
-    return JSON.parse(localStorage.getItem('user'));
+  getUser() : User {
+    return {
+      id: 0,
+      email: '',
+      nom: '',
+      prenom: '',
+      motdepasse: { pwd: '' }
+    };
+  }
+
+  getUsers(): User[] {
+    // Si JSON.parse(localStorage.getItem('users') est défini
+    // Alors je renvoie JSON.parse(localStorage.getItem('users')
+    // Sinon, je renvoie un tableau vide
+    const usersInLocalStorage = JSON.parse(localStorage.getItem('users'));
+    if (usersInLocalStorage) {
+      return usersInLocalStorage;
+    }else {
+      return [];
+    }
+  }
+
+  setUsers(users: User[]): void {
+    localStorage.setItem('users', JSON.stringify(users));
   }
 }
