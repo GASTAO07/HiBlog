8eb79c3addb688da0ffc18c9f4ca7574180f2417
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-02-21 14:08:04 +0100

    adding removeItem to make the id disappear when loggedOut and the user list

--- src/app/tool-bar/tool-bar.component.ts
+++ src/app/tool-bar/tool-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginValidationService } from '../service/auth-service/login-validation-service.service';
+import { AuthService } from '../service/auth-service/auth.service';
+import { UserService } from '../service/user.service';
+import { User } from '../interfaces/user.interface';
 
 @Component({
   selector: 'app-tool-bar',
@@ -9,21 +12,39 @@ import { LoginValidationService } from '../service/auth-service/login-validation
 })
 export class ToolBarComponent {
   isUserLoggedIn: boolean = false;
+  isValidBlog: boolean = true;
+  user: User;
 
   ngOnInit(): void {
+    const currentUserId = Number(localStorage.getItem('currentUserId'));
+    const currentUser: User = this.userservice.getUserById(currentUserId);
+    // Si currentUser est undefined, renvoyer une erreur
+    if (currentUser === undefined) {
+      return;
+    } else {
+      this.user = currentUser;
+    }
     this.isUserLoggedIn = this.loginValidationService.isLoggedIn();
   }
 
   constructor(
     private loginValidationService: LoginValidationService,
-    private router: Router) {}
+    private router: Router,
+    private auth: AuthService,
+    private userservice: UserService) {}
 
   goToBloglist(): void {
     this.router.navigateByUrl('listedeblogs');
   }
 
   logout() : void {
+    this.loginValidationService.isLoggedOut();
+    localStorage.removeItem('currentUserId');
     this.router.navigateByUrl('auth/login');
+  }
+
+  userList() : void {
+    this.router.navigateByUrl('userlist');
   }
 }
 
