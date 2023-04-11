df4405af140a9d2c8de1da1a8d1a252552a2c5c1
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-04-11 18:21:31 +0200

    children path declaration in auth.module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthRoutingModule } from './auth-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { CreerUncompteComponent } from '../creer-uncompte/creer-uncompte.component';
+import { AuthGuard } from '../guards/auth.guard';
+import { AuthLoginGuard } from '../guards/auth.loginguard';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, canActivate: [AuthLoginGuard] },
+  { path: 'creeruncompte', component: CreerUncompteComponent }
+];
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  providers: [AuthGuard, AuthLoginGuard]
 })
 export class AuthModule { }
