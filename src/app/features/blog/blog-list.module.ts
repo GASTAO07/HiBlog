eb48f4e993e34a6fc179384f080a4255642aab6d
Rogério GASTAO <100167018+GASTAO07@users.noreply.github.com>
2023-04-24 10:55:12 +0200

    adding the module to blog-list

--- src/app/features/blog/blog-list.module.ts
+++ src/app/features/blog/blog-list.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogListComponent } from './blog-list.component';
 import { PageBlogComponent } from './components/blog-creation-edit/page-blog.component';
+import { CreateCategoriesComponent } from './components/categories/create-categories.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/blog/listedeblogs'},
+  { path: '', pathMatch: 'full', redirectTo: '/blog/listedeblogs' },
+  { path: 'create-categories', component: CreateCategoriesComponent },
   { path: 'listedeblogs', component: BlogListComponent },
   { path: 'pageblog', component: PageBlogComponent },
-  { path: 'pageblog/:id', component: PageBlogComponent},
+  { path: 'pageblog/:id', component: PageBlogComponent },
 ];
 
 @NgModule({
