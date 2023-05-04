604d19dd2fbd40374e820de74cb7a4d26af07daa
GASTAO <rogerio.gastao@capgemini.com>
2023-05-04 15:17:07 +0200

    adding hashtags as string[] to the interface

--- src/app/features/blog/services/liste-blog-enregistres/liste-blog-enregistres.service.ts
+++ src/app/features/blog/services/liste-blog-enregistres/liste-blog-enregistres.service.ts
@@ -4,12 +4,13 @@ import { Category } from '../../interfaces/category.interface';
 import { IdmanagerService } from 'src/app/shared/services/idmanager.service';
 
 @Injectable()
-
 export class ListeBlogEnregistresService {
   private blogs: Blog[] = [];
   private categories: Category[] = [];
 
-  constructor(private idmanagerService : IdmanagerService) { console.log('Nouvelle instance de ListeBlogEnregistresService créée'); }
+  constructor(private idmanagerService: IdmanagerService) {
+    console.log('Nouvelle instance de ListeBlogEnregistresService créée');
+  }
 
   getBlogById(id: number): Blog {
     const index = this.idmanagerService.findIndexById(id, this.blogs);
@@ -20,10 +21,10 @@ export class ListeBlogEnregistresService {
     return this.blogs;
   }
 
-  modifyBlog(id: number, titre: string, description: string, category: Category): void {
+  modifyBlog(id: number, titre: string, description: string, category: Category, hashtags: string[]): void {
     const index = this.idmanagerService.findIndexById(id, this.blogs);
     if (index !== -1) {
-      this.blogs[index] = { id, titre, description, category };
+      this.blogs[index] = { id, titre, description, category, hashtags };
     } else {
       throw new Error('blog n\'existe pas');
     }
@@ -36,17 +37,25 @@ export class ListeBlogEnregistresService {
     }
   }
 
-  addBlog(titre: string, description: string, category: Category): void {
-    const id = this.idmanagerService.generateUniqueId(this.blogs.map((blog: Blog) : number => blog.id));
-
-    this.blogs.push({ id: id, titre: titre, description: description, category: category });
+  addBlog(titre: string, description: string, category: Category, hashtags: string[] = []): void {
+    // si tableau de hashtags sera utilisé pour le paramètre hashtags
+    // Sinon si le paramètre undefined tableau vide ([])
+    const id = this.idmanagerService.generateUniqueId(this.blogs.map((blog: Blog): number => blog.id));
+    this.blogs.push({ id: id, titre: titre, description: description, category: category, hashtags: hashtags });
   }
 
   duplicateBlog(id: number): void {
     const blog = this.getBlogById(id);
     if (blog) {
-      const newId = this.idmanagerService.generateUniqueId(this.blogs.map((blog: Blog) : number => blog.id));
-      const newBlog = { id: newId, titre: blog.titre, description: blog.description, category: blog.category };
+      const newId = this.idmanagerService.generateUniqueId(this.blogs.map((blog: Blog): number => blog.id));
+      const newBlog = {
+        id: newId,
+        titre: blog.titre,
+        description: blog.description,
+        category: blog.category,
+        hashtags: blog.hashtags || []
+        // si blog.hashtags a une valeur, cette valeur est retournée, sinon vide (null/undefined)
+      };
       this.blogs.push(newBlog);
     } else {
       throw new Error('blog n\'existe pas');
@@ -54,6 +63,6 @@ export class ListeBlogEnregistresService {
   }
 
   getBlogsByCategory(category: Category): Blog[] {
-    return this.blogs.filter((blog : Blog) : any => blog.category === category);
+    return this.blogs.filter((blog: Blog): any => blog.category === category);
   }
 }
